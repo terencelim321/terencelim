309cd267febf843decad42c1f4ef8a7c64223ac7
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-05-28 17:31:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, json
+from flask import Flask, request, jsonify, json, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from os import environ
@@ -27,8 +27,11 @@ class Sum(db.Model):
         self.Number1 = Number1
         self.Number2 = Number2
 
-    #def json(self):
-        #return {"AbandonID": self.AbandonID, "FirstName": self.FirstName, "LastName": self.LastName, "AbandonEmail": self.AbandonEmail, "TeleID": self.TeleID}
+@app.route('/')
+def main():
+    return render_template('login.html')
+
+
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
    if request.method == 'POST':
