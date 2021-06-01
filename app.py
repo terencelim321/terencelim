2d82e366bc1c6d3c066a05c74305595ef63d29b8
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-01 12:50:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, request, jsonify, json, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
-from os import environ
+import os
 
 app = Flask(__name__)
 #dev database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/postdata'
 #app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("dbURL")
 #production database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://bwtdvlbfpruyuc:a291b9482d49f67d638b5188f1934104dee5722cdc5facf539243ca05033a675@ec2-34-202-54-225.compute-1.amazonaws.com:5432/d4a674qjb8ea29'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 CORS(app)
 
