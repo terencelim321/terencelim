83c1b045316b27f374ad79cddfa70bdf0dcb4aac
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-09 13:43:43 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,57 +1,35 @@
-from flask import jsonify, abort, request, make_response
-#production database
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#CORS(app)
-from flask import Flask
+from flask import Flask, request, jsonify, json, render_template
 from flask_sqlalchemy import SQLAlchemy
-#from app_models import video_text
+from flask_cors import CORS
 import os
 
-
-app = Flask(__name__, instance_relative_config=True)
-
-
+app = Flask(__name__)
+#dev database
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/postdata'
+#app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("dbURL")
+#production database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("dbURL")
+CORS(app)
 
 db = SQLAlchemy(app)
 
-class video_text(db.Model):
-    __tablename__ = 'video_text'
+class Sum(db.Model):
+    __tablename__ = 'sum'
 
-    id = db.Column(db.Integer, primary_key=True)
-    video_id = db.Column(db.Integer, nullable=False)
-    segment_number = db.Column(db.Integer, nullable=False)
-    full_text = db.Column(db.String(2000), nullable=False)
-    starttime = db.Column(db.Time , nullable=False)
-    endtime = db.Column(db.Time , nullable=False)
+    Number1 = db.Column(db.Integer, primary_key=True)
+    Number2 = db.Column(db.Integer, primary_key=True)
+    
 
-    def __init__(self, id, video_id, segment_number, full_text, starttime, endtime):
-        self.id = id
-        self.video_id= video_id
-        self.segment_number = segment_number
-        self.full_text = full_text
-        self.starttime = starttime
-        self.endtime = endtime
+    def __init__(self, Number1, Number2):
+        self.Number1 = Number1
+        self.Number2 = Number2
 
+    
 @app.route('/')
 def main():
-
-    id = 1
-    video_id = 1
-    segment_number = 1
-    full_text = 'hello'
-    starttime="00:00:00"
-    endtime = "00:00:59"
-    if db.session.query(video_text).filter(video_text.id == id).count() == 0:
-        data = video_text(id, video_id,segment_number,full_text,starttime,endtime)
-        db.session.add(data)
-        db.session.commit()
+    #return render_template('login.html')
     return "hello"
-    #return render_template('login.html')e
 
 @app.route('/login',methods = ['POST', 'GET'])
 def login():
@@ -66,10 +44,10 @@ def login():
       Number2 = request.args.get('num2')
       sum = int(Number1) + int(Number2)
 
-      #if db.session.query(numbers).filter(numbers.Number1 == Number1).count() == 0:
-          #data = numbers(Number1, Number2)
-          #db.session.add(data)
-          #db.session.commit()
+      if db.session.query(Sum).filter(Sum.Number1 == Number1).count() == 0:
+          data = Sum(Number1, Number2)
+          db.session.add(data)
+          db.session.commit()
 
       return (str(sum) + ' is the total sum of these two numbers')
 
