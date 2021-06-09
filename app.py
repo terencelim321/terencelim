ae8b14c93413c03740ea613fb252e84d968ac291
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-09 13:36:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
 #CORS(app)
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from app_models import video_text
 import os
 
 
@@ -19,4 +19,59 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-    
+class video_text(db.Model):
+    __tablename__ = 'video_text'
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, nullable=False)
+    segment_number = db.Column(db.Integer, nullable=False)
+    full_text = db.Column(db.String(2000), nullable=False)
+    starttime = db.Column(db.Time , nullable=False)
+    endtime = db.Column(db.Time , nullable=False)
+
+    def __init__(self, id, video_id, segment_number, full_text, starttime, endtime):
+        self.id = id
+        self.video_id= video_id
+        self.segment_number = segment_number
+        self.full_text = full_text
+        self.starttime = starttime
+        self.endtime = endtime
+
+@app.route('/')
+def main():
+
+    id = 1
+    video_id = 1
+    segment_number = 1
+    full_text = 'hello'
+    starttime="00:00:00"
+    endtime = "00:00:59"
+    if db.session.query(video_text).filter(video_text.id == id).count() == 0:
+        data = video_text(id, video_id,segment_number,full_text,starttime,endtime)
+        db.session.add(data)
+        db.session.commit()
+    return "hello"
+    #return render_template('login.html')e
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      num1 = request.form['num1']
+      num2 = request.form['num2']
+
+      sum = int(num1) + int(num2)
+      return str(sum)
+   else:
+      Number1 = request.args.get('num1')
+      Number2 = request.args.get('num2')
+      sum = int(Number1) + int(Number2)
+
+      if db.session.query(numbers).filter(numbers.Number1 == Number1).count() == 0:
+          data = numbers(Number1, Number2)
+          db.session.add(data)
+          db.session.commit()
+
+      return (str(sum) + ' is the total sum of these two numbers')
+
+if __name__ == '__main__':
+   app.run(debug = True)
