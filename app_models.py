86def73b1f6e3882dc0c648a7f4ebaf00e277bce
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-09 13:18:19 +0800

    Create app_models.py

---
+++ app_models.py
@@ -0,0 +1,33 @@
+#import datetime
+#from flask import Flask, request, jsonify, json, render_template
+from flask_sqlalchemy import SQLAlchemy
+#from flask_cors import CORS
+#import os 
+#from app_routes import app as app
+
+
+db = SQLAlchemy()
+
+class video_text(db.Model):
+    __tablename__ = 'video_text'
+
+    id = db.Column(db.Integer, primary_key=True)
+    video_id = db.Column(db.Integer, nullable=False)
+    segment_number = db.Column(db.Integer, nullable=False)
+    full_text = db.Column(db.String(2000), nullable=False)
+    starttime = db.Column(db.Time , nullable=False)
+    endtime = db.Column(db.Time , nullable=False)
+
+    def __init__(self, id, video_id, segment_number, full_text, starttime, endtime):
+        self.id = id
+        self.video_id= video_id
+        self.segment_number = segment_number
+        self.full_text = full_text
+        self.starttime = starttime
+        self.endtime = endtime
+
+
+
+
+
+    
