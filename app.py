6337d007e21491409c83a0a99bab49e5fbde3d84
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-09 13:18:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,54 +1,22 @@
-from flask import Flask, request, jsonify, json, render_template
+
+#production database
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#CORS(app)
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_cors import CORS
+
 import os
 
-app = Flask(__name__)
-#dev database
+
+app = Flask(__name__, instance_relative_config=True)
+
+
 #app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root@localhost:3306/postdata'
-#app.config['SQLALCHEMY_DATABASE_URI'] = environ.get("dbURL")
-#production database
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-CORS(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("dbURL")
 
 db = SQLAlchemy(app)
 
-class Sum(db.Model):
-    __tablename__ = 'sum'
-
-    Number1 = db.Column(db.Integer, primary_key=True)
-    Number2 = db.Column(db.Integer, primary_key=True)
     
-
-    def __init__(self, Number1, Number2):
-        self.Number1 = Number1
-        self.Number2 = Number2
-
-    
-@app.route('/')
-def main():
-    return render_template('login.html')
-
-@app.route('/login',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      num1 = request.form['num1']
-      num2 = request.form['num2']
-
-      sum = int(num1) + int(num2)
-      return str(sum)
-   else:
-      Number1 = request.args.get('num1')
-      Number2 = request.args.get('num2')
-      sum = int(Number1) + int(Number2)
-
-      if db.session.query(Sum).filter(Sum.Number1 == Number1).count() == 0:
-          data = Sum(Number1, Number2)
-          db.session.add(data)
-          db.session.commit()
-
-      return (str(sum) + ' is the total sum of these two numbers')
-
-if __name__ == '__main__':
-   app.run(debug = True)
