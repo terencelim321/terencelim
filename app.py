2c917e22ef5b77e9316593732350eee4c96ac88e
Terence321 <terence.lim.2019@sis.smu.edu.sg>
2021-06-10 13:52:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ class video_text(db.Model):
     
 @app.route('/',methods = ['POST', 'GET'])
 def main():
-    
+    from nltk.corpus import stopwords
     #from nltk.corpus import stopwords
     
     
@@ -62,23 +62,23 @@ def main():
         
     #all_text_json = json.dumps(all_text)
     
-    stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 
-    'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
-    'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', 
-    "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 
-    'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 
-    'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 
-    'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 
-    'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 
-    'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how',
-     'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 
-     'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', 
-     "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 
-     'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', 
-     "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn',
-      "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+    #stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 
+    #'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
+    #'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', 
+    #"it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 
+    #'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 
+    #'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 
+    #'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 
+    #'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 
+    #'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how',
+    # 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 
+    # 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', 
+    # "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 
+    # 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', 
+    # "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn',
+    #  "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
     # Create stopword list:
-    #stopwords= stopwords.words("english")
+    stopwords= set(stopwords.words("english"))
     #sets all stop words
     #stopwords.add('(')
     #stopwords.add(')')
@@ -92,19 +92,19 @@ def main():
     #stopwords.add('""')
     #stopwords.add("n't")
     #stopwords.add("'re")
-
-    stopwords.append('(')
-    stopwords.append(')')
-    stopwords.append('[')
-    stopwords.append(']')
-    stopwords.append("``")
-    stopwords.append("''")
-    stopwords.append(",")
-    stopwords.append("'s")
-    stopwords.append("'")
-    stopwords.append('""')
-    stopwords.append("n't")
-    stopwords.append("'re")
+    
+    #stopwords.append('(')
+    #stopwords.append(')')
+    #stopwords.append('[')
+    #stopwords.append(']')
+    #stopwords.append("``")
+    #stopwords.append("''")
+    #stopwords.append(",")
+    #stopwords.append("'s")
+    #stopwords.append("'")
+    #stopwords.append('""')
+    #stopwords.append("n't")
+    #stopwords.append("'re")
 
     tokenized_word = word_tokenize(str(all_text))
     #print(tokenized_word)
@@ -122,7 +122,8 @@ def main():
 
 
     freq_json = json.dumps(top_10)
-    return  freq_json
+    #return  freq_json
+    return  stopwords
 
 
 
